f64e730edd7fb228f9f498e728296deda72d1f34
vishalteckwani94 <66530186+vishalteckwani94@users.noreply.github.com>
2022-03-31 16:35:18 +0530

    Create HelloRestController.java

---
+++ src/main/java/com/zensar/HelloRestController.java
@@ -0,0 +1,11 @@
+package com.zensar;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+@RestController
+public class HelloRestController{
+  @GetMapping("/Hello")
+  public String getmessage()
+  {
+    return "Welcome to .......................";
+  }
+}
